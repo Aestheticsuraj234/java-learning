3aa3f0a38f57abf2d566a95ebeb2e172f2a4a313
Aestheticsuraj234 <jhapriya458@gmail.com>
2026-05-19 02:19:56 +0530

    Promoting

--- Hello.java
+++ Hello.java
@@ -1,21 +1,11 @@
 public class Hello {
     public static void main(String[] args) {
-       int num1 = 10;
-       int num2 = 20;
+       byte b = 127;
+       int a = b;
+        
 
-       int sum = num1 + num2;
 
-       float num3 = 10.5f;
-       float sum2 = num1 + num3;
-
-        char char1 = 'A';
-        char char2 = 'B';
-        char2 = char1; // implicit casting
-        char sum3 = (char) (char1 + char2); // type casting ( explicit casting )
-
-        System.out.println("Sum of " + num1 + " and " + num2 + " is " + sum);
-        System.out.println("Sum of " + num1 + " and " + num3 + " is " + sum2);
-        System.out.println("Sum of " + char1 + " and " + char2 + " is " + sum3);
+       System.out.println(a);
 
     }
 }
