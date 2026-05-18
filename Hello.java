13191e284f4f7e068533e8cfeed0f2b67a8ee8b6
Aestheticsuraj234 <jhapriya458@gmail.com>
2026-05-19 02:15:49 +0530

    variables and typecasting

--- Hello.java
+++ Hello.java
@@ -1,5 +1,27 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+       int num1 = 10;
+       int num2 = 20;
+
+       int sum = num1 + num2;
+
+       float num3 = 10.5f;
+       float sum2 = num1 + num3;
+
+        char char1 = 'A';
+        char char2 = 'B';
+        char2 = char1; // implicit casting
+        char sum3 = (char) (char1 + char2); // type casting ( explicit casting )
+
+        System.out.println("Sum of " + num1 + " and " + num2 + " is " + sum);
+        System.out.println("Sum of " + num1 + " and " + num3 + " is " + sum2);
+        System.out.println("Sum of " + char1 + " and " + char2 + " is " + sum3);
+
     }
 }
+
+// Primitive Data types
+// 1. int (byte , short , int , long)
+// 2. float (float , double)
+// 3. char (char)
+// 4. boolean (boolean)
