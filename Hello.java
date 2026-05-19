f81772c7a9a724b7a8b1e86cbf6b698af2fa547d
Aestheticsuraj234 <jhapriya458@gmail.com>
2026-05-19 10:40:32 +0530

    do while  loop

--- Hello.java
+++ Hello.java
@@ -1,18 +1,13 @@
 public class Hello {
     public static void main(String[] args) {
     // While Loop
+        
+    // do while loop
     int i = 1;
-    while(i <= 10){
-      System.out.println("Hello" + i);
-        int j = 1;
-      while(j<=3){
-        System.out.println("World" + j);
-        j++;
-      }
-
-      System.out.println();
-      i++;
-    }
+    do{
+        System.out.println("Hello" + i);
+        i++;
+    }while(i <= 10);
 
     }
 }
