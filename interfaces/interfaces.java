8bc9cac13b9aa487fe7c4fe3d7d2ea624c758e35
Aestheticsuraj234 <jhapriya458@gmail.com>
2026-05-22 16:25:58 +0530

    Lamba interface

--- interfaces/interfaces.java
+++ interfaces/interfaces.java
@@ -2,13 +2,15 @@ package interfaces;
 
 @FunctionalInterface
 interface A {
-    void methodA(int i , int j);
+    int methodA(int i , int j);
 }
 
 
 public class interfaces {
     public static void main(String[] args) {
-        A a = ( i , j) -> System.out.println("Hello from methodA!");
-        a.methodA(5, 10);
+            A a = ( i , j) -> i+j;
+            
+            int result = a.methodA(5, 10);
+            System.out.println("Result: " + result);
     }
 }
